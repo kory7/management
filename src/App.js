84a85cc928492ac35d2d2f156fd944c3be2aa763
Jony Choi <jony.choi7@gmail.com>
2019-05-24 01:16:31 -0500

    Create customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from "react";
 import "./App.css";
-
-function App() {
-  return (
-    <div className="gray-background">
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+import Customer from "./components/Customer";
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/any",
+    name: "Jung Ik",
+    birthday: "920311",
+    gender: "Male",
+    job: "Developer"
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/any",
+    name: "Jony",
+    birthday: "920311",
+    gender: "Male",
+    job: "Developer"
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/any",
+    name: "Juan",
+    birthday: "920311",
+    gender: "Male",
+    job: "Developer"
+  }
+];
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {customers.map(c => {
+          return (
+            <Customer
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              id={c.id}
+              key={c.id}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
